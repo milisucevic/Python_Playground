0860498919ccfc7928251c07009565bf27f81b8a
Milica Sucevic <milicasucevic@gmail.com>
2019-04-10 11:08:51 +0100

    Initial Commit

---
+++ simple_nameage.py
@@ -0,0 +1,4 @@
+Name = input("Give me your name: ") #To get user input, the command you use is input()
+Age = input("Enter your age: ")
+print("Your name is: " + name + " and your age is: " + str(age))
+
