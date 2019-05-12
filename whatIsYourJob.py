6e0f97398f366b8e2c6dae0e3736b34991ef6ee7
Milica Sucevic <milicasucevic@gmail.com>
2019-05-12 15:41:42 +0100

    initial commit

---
+++ whatIsYourJob.py
@@ -0,0 +1,10 @@
+#This program says hello and asks for your job
+print('Hello, stranger!')
+print('What is your name?')
+myName = input()
+print('Welcome on board, ' + str(myName)) 
+print('Tell me more details about you ' + str(myName) + '. What is your job?')
+myJob = input()
+print(str(myJob) + ', Nice one! How long have you been doing that for?')
+jobAge = input()
+print('Wow ' + str(int(jobAge)) + ' years! You are becoming a part of the furniture!')  
