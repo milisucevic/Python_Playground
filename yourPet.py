c5b18409dd6c0b34b8af746c89128b8b841857c9
Milica Sucevic <milicasucevic@gmail.com>
2019-05-12 15:48:21 +0100

    initial commit

---
+++ yourPet.py
@@ -0,0 +1,9 @@
+#This program says hello and asks about your pet
+print('Welcome to my planet stranger, what is your name?')
+myName = input()
+print('Nice to meet you ' + myName + '. Who do you have with you today?')
+myPet = input()
+print(str(myPet) + ', awwww such a lovely name, how old is ' + str(myPet) + ' ?')
+myPetAge = input()
+print(str(int(myPetAge)) + ' , so cute!')
+
